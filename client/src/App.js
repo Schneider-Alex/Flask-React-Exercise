2c918eb51ea0f140405fd1c77d1c4554752bd8c1
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-07-18 12:39:17 -0400

    Simple useState example for increasing/decreasing button

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import React  from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   const [data, setData] = useState([{}]);
 
   useEffect(()=>{
@@ -16,14 +17,15 @@ function App() {
   },[]);
   return (
     <div>
-
     {(typeof data.members === 'undefined') ? (
       <p>Loading...</p>
     ):(data.members.map((member,i)=>(
       <p key={i}>{member}</p>
     ))
     )}
-
+    <p>{count}</p>
+    <button onClick={() => setCount(count+1)}>Press me to increase count</button>
+    <button onClick={() => setCount(count-1)}>Press me to decrease count</button>
     </div>
   )
 }
